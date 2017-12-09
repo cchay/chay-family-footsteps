8ea2271d03310f2c6209fa846a84d9ab730dd133
Hacker <hacker@hacker-desktop>
2017-12-09 14:42:04 -0500

    Player:testing player.py

--- Player/PlayerTest.py
+++ Player/PlayerTest.py
@@ -1,9 +1,9 @@
 import Player
 
-p = Player.Player().GetNewPlayer( 'Jack', 'Male' )
+p = Player.Player().GetNewPlayer( 'Jack', 'Female' )
 p.ShowPlayerInfo()
 
 print( "\n" )
 
-p1 = Player.Player().GetNewPlayer( 'Jane', 'Female' )
+p1 = Player.Player().GetNewPlayer( 'Jane', 'Male' )
 p1.ShowPlayerInfo()
