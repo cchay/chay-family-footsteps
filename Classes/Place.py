75ee685983de3bb16817d3e1553cd142362cf343
christopherchay <chay.c2.christopher@gmail.com>
2017-12-10 18:32:59 -0500

    C2, C2: Created the Place Class and the Town sub-Class

--- Classes/Place.py
+++ Classes/Place.py
@@ -5,3 +5,69 @@ class Place:
 		self.Description = ''
 		self.Name = ''
 		self.CanHaveBuildings = False
+	
+	def GetID(self):
+		return self.ID
+	
+	def SetID(self, Id):
+		self.ID = Id
+	
+	
+	def GetType(self):
+		return self.Type
+	
+	def GetType(self, Type):
+		self.Type = Type
+		
+	
+	def GetDescription(self):
+		return self.Description
+	
+	def GetDescription(self, Id):
+		self.ID = Id
+	
+	
+	def GetName(self):
+		return self.Name
+	
+	def GetName(self, name):
+		self.Name = name
+	
+	
+	def GetCanHaveBuildings(self):
+		return self.ID
+	
+	def GetCanHaveBuildings(self, canhavebuildings):
+		self.GetCanHaveBuildings = canhavebuildings
+
+
+
+
+
+class Town(Place):
+	def __init__(self):
+		self.CanGoTo = ''
+	
+	def GetCanGoTo(self):
+		return self.CanGoTo
+
+	def SetCanGoTo(self, cangoto):
+		self.CanGoTo = cangoto
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
