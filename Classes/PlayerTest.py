10a5157985c1a40e96c9d785a47129ede6d670e9
Chris Chay <chay.c0.chris@gmail.com>
2017-12-09 20:54:39 -0500

    master: changed name of folder Class to Classess

---
+++ Classes/PlayerTest.py
@@ -0,0 +1,9 @@
+import Player
+
+p = Player.Player().GetNewPlayer( '', 'FemaleFemale' )
+p.ShowPlayerInfo()
+
+print( "\n" )
+
+p1 = Player.Player().GetNewPlayer( '', 'MaleMale' )
+p1.ShowPlayerInfo()
