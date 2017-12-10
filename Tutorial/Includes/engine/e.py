b279ac50638d5eab8af5cd29e6425aafde4d0499
Chris Chay <chay.c0.chris@gmail.com>
2017-12-09 20:27:42 -0500

    Player: added Tutorial for including files

---
+++ Tutorial/Includes/engine/e.py
@@ -0,0 +1,6 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'Classes/SubClasses'))
+import SubPlayer
+
+
+SubPlayer.subp()
---
+++ Tutorial/Includes/main.py
@@ -0,0 +1,5 @@
+from Classes import Player
+from Classes.SubClasses import SubPlayer
+
+Player.p()
+SubPlayer.subp()
