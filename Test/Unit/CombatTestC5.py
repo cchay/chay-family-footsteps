03c3c162fb61125e63289c91aaca2e33f567dfee
celestachay <chay.c5.celesta@gmail.com>
2017-12-10 20:04:06 -0500

    C5: test

---
+++ Test/Unit/CombatTestC5.py
@@ -0,0 +1,29 @@
+import sys, os
+
+## Import for Windows
+if os.name == 'nt':
+	sys.path.append(os.path.join(os.path.dirname(__file__), '..\..', 'Classes'))
+	
+	sys.path.append(os.path.join(os.path.dirname(__file__), '..\..', 'Engine'))
+## Import for Linux
+else:
+	sys.path.append(os.path.join(os.path.dirname(__file__), '../..', 'Classes'))
+	sys.path.append(os.path.join(os.path.dirname(__file__), '../..', 'Engine'))
+
+
+import Player
+import Combat
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..\..', 'Engine'))
+import Combat
+
+p1 = Player.Player().GetNewPlayer( 'Jane', 'Female' )
+p1.ShowPlayerInfo()
+
+p2 = Player.Player().GetNewPlayer( 'Jack', 'Male' )
+p2.ShowPlayerInfo()
+
+Combat.ResolveCombat( p1, p2 )
+
+print("The Loser's side of the fight is futile!")
