420eba4cac178f78048be0abf54fc0d9af22c06e
Hacker <hacker@hacker-desktop>
2017-12-12 13:44:34 -0500

    C4:Testing cchay's file

--- Test/Unit/PlayerTest.py
+++ Test/Unit/PlayerTest.py
@@ -2,10 +2,10 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..\..', 'Classes'))
 import Player
 
-p = Player.Player().GetNewPlayer( '', 'FemaleFemale' )
+p = Player.Player().GetNewPlayer( '', 'ALIEN')
 p.ShowPlayerInfo()
 
 print( "\n" )
 
-p1 = Player.Player().GetNewPlayer( '', 'MaleMale' )
+p1 = Player.Player().GetNewPlayer( '', 'ALIEN!!' )
 p1.ShowPlayerInfo()
