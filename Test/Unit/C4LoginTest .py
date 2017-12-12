760f4377ca244cbe2579c0f528229bdfc422d82e
Hacker <hacker@hacker-desktop>
2017-12-12 16:51:18 -0500

    Copied Test files

---
+++ Test/Unit/C4LoginTest .py
@@ -0,0 +1,9 @@
+import sys, os
+sys.path.append( os.path.join( os.path.dirname( __file__ ), '../..', 'Login' ) )
+import Login
+
+## Run the Register Function
+## The Player json files get written and created locally in the same folder
+
+Login.Register()
+
