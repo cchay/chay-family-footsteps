a3505935456a47715f825fb87795f894460eb5ef
Hacker <hacker@hacker-desktop>
2017-12-18 19:25:03 -0500

    C4:Testing

--- Test/Unit/C4PlayerTest .py
+++ Test/Unit/C4PlayerTest .py
@@ -2,10 +2,11 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..\..', 'Classes'))
 import Player
 
-p2. = Player.Player().GetNewPlayer( '', 'ALIEN')
+
+p2 = Player.Player().GetNewPlayer( '', '')
 p2.ShowPlayerInfo()
 
 print( "\n" )
 
-p1 = Player.Player().GetNewPlayer( 'Thor', 'Ma' )
+p1 = Player.Player().GetNewPlayer( '', '' )
 p1.ShowPlayerInfo()
